077ac00099425eff7af953a86b1c74f45990ed92
Ion Obreja <jo@da.md>
2022-11-21 12:30:19 +0200

    Change page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `http://astro.build`,
+  integrations: [
+    preact(),
+    react(),
+  ],
+  site: 'https://joSecret.github.io',
+  base: '/docs',
 });
