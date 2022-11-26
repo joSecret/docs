a992db0a4dded0fde697ad10aa9800df03f73b5a
Ion Obreja <jo@da.md>
2022-11-26 16:31:11 +0200

    Change edit link.

--- src/config.ts
+++ src/config.ts
@@ -28,9 +28,10 @@ export type Frontmatter = {
 export const KNOWN_LANGUAGES = {
   English: 'en',
 } as const;
+
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/joSecret/docs/blob/main`;
 
 export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
